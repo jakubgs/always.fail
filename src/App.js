446b1fc89f3c1a2c0ca00caada01ceb54eea9559
Jakub Sokołowski <panswiata@gmail.com>
2017-10-30 17:03:04 +0000

    adjust code to match domain name

--- src/App.js
+++ src/App.js
@@ -9,15 +9,16 @@ function capitalize(text) {
 class App extends Component {
   render() {
     const tokens = window.location.hostname.split('.');
-    let subject = 'Something will';
+    let subject = 'Something';
     if (tokens.length === 3) {
       subject = capitalize(tokens[0]);
     }
+    let ending = subject.endsWith('s') ? 'fail' : 'fails';
     return (
       <div className="App">
         <header className="App-header">
-          <img src={fail} className="fail-mark" alt="fail mark" />
-          <h1 className="App-title">{subject} always fail.</h1>
+          <img src={fail} className="fail-mark" alt="fail mark"/>
+          <h1 className="App-title">{subject} will always {ending}.</h1>
         </header>
       </div>
     );
