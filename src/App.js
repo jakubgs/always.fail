ab6b1ebcafe217e6623f9119627794514e51d11f
Jakub Sokołowski <panswiata@gmail.com>
2017-10-30 21:23:31 +0000

    add title setting

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Helmet} from "react-helmet";
 import './bootstrap.min.css';
 import './App.css';
 import fail from './fail.png';
@@ -28,11 +29,15 @@ class App extends Component {
 
   render () {
     let quote = this.randomQuote();
+    let subject = this.getSubject();
     return (
       <div className="App">
+        <Helmet>
+          <title>{subject}</title>
+        </Helmet>
         <header className="App-header">
           <img src={fail} className="logo" alt="fail mark"/>
-          <h1 className="App-title">{this.getSubject()}</h1>
+          <h1 className="App-title">{subject}</h1>
         </header>
         <br/>
         <div className="quote">
